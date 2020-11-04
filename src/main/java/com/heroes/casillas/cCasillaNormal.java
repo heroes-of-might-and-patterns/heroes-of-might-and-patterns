7e5b051f01414cb3f17704d532f56b78c89cd848
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-11-04 00:14:35 -0600

    crear clase de casilla normal

---
+++ src/main/java/com/heroes/casillas/cCasillaNormal.java
@@ -0,0 +1,21 @@
+package com.heroes.casillas;
+
+import com.heroes.castillo.Castillo;
+
+public class cCasillaNormal {
+	private Castillo castillo;
+
+	public cCasillaNormal(){}
+
+	public Castillo getCastillo() {
+		return castillo;
+	}
+
+	public void setCastillo(Castillo castillo) {
+		this.castillo = castillo;
+	}
+
+	public boolean estaVacia(){
+		return this.castillo == null;
+	}
+}
