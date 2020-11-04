33a805e5a39f3e3ebf9bdee0a136773fb7d62649
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-11-04 00:15:02 -0600

    crear clase abstracta de decorador de casilla

---
+++ src/main/java/com/heroes/casillas/aDecoradorCasilla.java
@@ -0,0 +1,11 @@
+package com.heroes.casillas;
+
+public abstract class aDecoradorCasilla {
+	protected cCasillaNormal casilla;
+
+	public aDecoradorCasilla() {
+		this.casilla = new cCasillaNormal();
+	}
+
+	public abstract void pisada();
+}
