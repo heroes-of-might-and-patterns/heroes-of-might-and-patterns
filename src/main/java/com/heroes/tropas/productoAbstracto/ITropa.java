be5ab4b90c31e541ab8130428ba659f960978ee2
jdiego98 <jarguelloq@ucenfotec.ac.cr>
2020-11-10 22:52:21 -0600

    Implementacion de atacar, defender y robar oro

--- src/main/java/com/heroes/tropas/productoAbstracto/ITropa.java
+++ src/main/java/com/heroes/tropas/productoAbstracto/ITropa.java
@@ -1,7 +1,8 @@
 package com.heroes.tropas.productoAbstracto;
 
 public interface ITropa {
-    public void atacar(ITropa objetivo);
+    public ITropa atacar(ITropa objetivo);
     public void moverse(int cordX, int cordY);
     public void robarOro(int oro);
+    public void defender(int ataque);
 }
--- src/main/java/com/heroes/tropas/productoConcreto/Jinete.java
+++ src/main/java/com/heroes/tropas/productoConcreto/Jinete.java
@@ -10,12 +10,78 @@ public class Jinete implements ITropa {
     private int defensa;
     private int ptsAtaque;
     private int ptsAlcance;
-    private int oroTrasportable;
+    private int oroTransportado;
     private String nombre;
 
-    @Override
-    public void atacar(ITropa objetivo) {
 
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getCantMoviminetos() {
+        return cantMoviminetos;
+    }
+
+    public void setCantMoviminetos(int cantMoviminetos) {
+        this.cantMoviminetos = cantMoviminetos;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public int getPtsAtaque() {
+        return ptsAtaque;
+    }
+
+    public void setPtsAtaque(int ptsAtaque) {
+        this.ptsAtaque = ptsAtaque;
+    }
+
+    public int getPtsAlcance() {
+        return ptsAlcance;
+    }
+
+    public void setPtsAlcance(int ptsAlcance) {
+        this.ptsAlcance = ptsAlcance;
+    }
+
+    public int getOroTransportado() {
+        return oroTransportado;
+    }
+
+    public void setOroTransportado(int oroTransportado) {
+        this.oroTransportado = oroTransportado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public ITropa atacar(ITropa objetivo) {
+        objetivo.defender(this.getPtsAtaque());
+        return objetivo;
     }
 
     @Override
@@ -25,6 +91,19 @@ public class Jinete implements ITropa {
 
     @Override
     public void robarOro(int oro) {
+        if(oro <= 6 && oro > 0){
+            this.setOroTransportado(oro);
+        }
+    }
 
+    @Override
+    public void defender(int ataque) {
+
+        if(this.getDefensa() < ataque){
+            this.setDefensa( this.defensa - ataque );
+        }else {
+            this.setVida( ataque - this.getDefensa() );
+            this.setDefensa(0);
+        }
     }
 }
