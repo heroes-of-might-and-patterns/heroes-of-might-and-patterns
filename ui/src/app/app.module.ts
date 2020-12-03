7a7720990b65221e019729b9186b19aac22a11b4
Cristopher Soto <csc8848@gmail.com>
2020-12-02 22:28:12 -0600

    adelanto de UI

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,23 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-
+import { MatGridListModule } from '@angular/material/grid-list';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { DadoComponent } from './utils/dado/dado.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableroComponent } from './tablero/tablero.component';
+import { FooterComponent } from './footer/footer.component';
+import { TropasJugadorComponent } from './tropas-jugador/tropas-jugador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DadoComponent
+    DadoComponent,
+    TableroComponent,
+    FooterComponent,
+    TropasJugadorComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatGridListModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ui/src/app/tropas-jugador/tropas-jugador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tropas-jugador',
+  templateUrl: './tropas-jugador.component.html',
+  styleUrls: ['./tropas-jugador.component.css']
+})
+export class TropasJugadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
