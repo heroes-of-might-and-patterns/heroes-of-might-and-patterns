ab46da0b1846c96659b4375147fe78e6570d4b8a
Eduardo Quiros <eduardo@eduardoquiros.com>
2020-12-07 00:38:26 -0600

    distribuir casillas especiales en el tablero

--- src/main/java/com/heroes/patterns/BoardController.java
+++ src/main/java/com/heroes/patterns/BoardController.java
@@ -8,25 +8,42 @@ import com.heroes.casillas.decoradores.cCasillaTrampaAtaque;
 import com.heroes.casillas.decoradores.cCasillaTrampaDefensa;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 	private final cCasillaNormal[] casillas = new cCasillaNormal[100];
 
-	public void decorarCasilla(int x, String tipo) {
+	@RequestMapping(value = "/iniciarTablero", method = RequestMethod.POST)
+	public void iniciarTablero() {
+		ArrayList<Integer> casillasEspeciales = new ArrayList<>();
+		while (casillasEspeciales.size() < 15) {
+			int numero = (int) (Math.random() / 100);
+			if (!casillasEspeciales.contains(numero))
+				casillasEspeciales.add(numero);
+		}
+		for (int i = 0; i < casillas.length; i++) {
+			if (casillasEspeciales.contains(i))
+				decorarCasilla(i, (int) (Math.random() / 4) + 1);
+		}
+	}
+
+	public void decorarCasilla(int x, int tipo) {
 		cCasillaNormal casilla = this.casillas[x];
 		switch (tipo) {
-			case "downDef":
+			case 0:
 				decorarCasilla(x, new cCasillaTrampaDefensa(casilla));
 				break;
-			case "upDef":
+			case 1:
 				decorarCasilla(x, new cCasillaPowerUpDefensa(casilla));
 				break;
-			case "downAtk":
+			case 2:
 				decorarCasilla(x, new cCasillaTrampaAtaque(casilla));
 				break;
-			case "upAtk":
+			case 3:
 				decorarCasilla(x, new cCasillaPowerUpAtaque(casilla));
 				break;
 		}
